19742992f736be8cec6d2bef03a35cf3e5fb81c3
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-30 13:58:12 +0530

    Empty button added to clear the todolist

--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import ToDoItem from "./ToDoItem";
 
 export default function ToDoList() {
-    const tasks = [
+    const initialTasks = [
         "Read SpringBoot",
         "Complete assignments",
         "Prepare breakfast",
         "Sleep for 2 hours",
         "Take a shower",
     ];
+    const [tasks, setTasks] = useState(initialTasks);
+    const clearToDoList = () => {
+        setTasks([]);
+    };
     return (
-        <ul className="to-do-list">
-            {tasks.map((task, index) => (
-                <ToDoItem key={index} data={task} />
-            ))}
-        </ul>
+        <>
+            <ul className="to-do-list">
+                {tasks.length === 0 && (
+                    <li className="empty-list-item">
+                        Nothing to do buddy. Sleep!
+                    </li>
+                )}
+                {tasks.map((task, index) => (
+                    <ToDoItem key={index} data={task} />
+                ))}
+            </ul>
+            <button
+                type="button"
+                className="empty-button"
+                onClick={clearToDoList}
+                disabled={tasks.length === 0}
+            >
+                Empty
+            </button>
+        </>
     );
 }
